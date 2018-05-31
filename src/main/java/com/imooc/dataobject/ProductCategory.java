61c3f7fc967e31f0c8f60c113bb2fc62cb992e6f
wangyu <francis_wangyu@163.com>
2018-05-31 22:45:29 +0800

    添加lombok自动生成getter/setter/toString方法

--- src/main/java/com/imooc/dataobject/ProductCategory.java
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -1,5 +1,6 @@
 package com.imooc.dataobject;
 
+import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import java.util.Date;
 //@Table(name = "product_category")
 @Entity
 @DynamicUpdate    //比如数据库自己设置的时间，如果更新的话，那么时间会自动更新
+@Data
 public class ProductCategory {
 
     /** 类目id. */
@@ -26,46 +28,6 @@ public class ProductCategory {
     private Date createTime;
 
     private Date updateTime;
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
 
     @Override
     public String toString() {
