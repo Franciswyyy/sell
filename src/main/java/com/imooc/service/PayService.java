2a9074740b0e391e6da452498abcf549cb8aa7e2
wangyu <francis_wangyu@163.com>
2018-06-09 09:47:30 +0800

    .

--- src/main/java/com/imooc/service/PayService.java
+++ src/main/java/com/imooc/service/PayService.java
@@ -3,6 +3,7 @@ package com.imooc.service;
 
 import com.imooc.dto.OrderDTO;
 import com.lly835.bestpay.model.PayResponse;
+import com.lly835.bestpay.model.RefundResponse;
 
 // 支付
 public interface PayService {
@@ -10,4 +11,6 @@ public interface PayService {
     PayResponse create(OrderDTO orderDTO);
     //异步通知
     PayResponse notify(String notifyData);
+
+    RefundResponse refund(OrderDTO orderDTO);
 }
