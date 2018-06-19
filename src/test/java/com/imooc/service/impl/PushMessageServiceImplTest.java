78e9e18ec163d6dbb62e331465e2d95164a18be4
wangyu <francis_wangyu@163.com>
2018-06-19 14:47:44 +0800

    模板消息推送

---
+++ src/test/java/com/imooc/service/impl/PushMessageServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.imooc.service.impl;
+
+import com.imooc.dto.OrderDTO;
+import com.imooc.service.OrderService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class PushMessageServiceImplTest {
+
+    @Autowired
+    private PushMessageServiceImpl pushMessageService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void orderStatus() throws Exception {
+
+        OrderDTO orderDTO = orderService.findOne("1528680782312359212");
+        pushMessageService.orderStatus(orderDTO);
+    }
+
+}
